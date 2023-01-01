dad07a2d0334d376475e8ee0897d6704efa69cab
xkh01 <xkh0917@gmail.com>
2023-01-01 11:49:26 +0800

    B3627 立方根

---
+++ Luogu/B3627.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+using std::cin;
+using std::cout;
+const char endl='\n';
+ll n;
+int main(){
+    std::ios::sync_with_stdio(0);
+    cin.tie(nullptr),cout.tie(nullptr);
+    cin>>n;
+    cout<<floor(cbrt(n))<<endl;
+    return 0;
+}
